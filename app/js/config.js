1c28aac78812d32cc741d35a519ac8b1f4d317f2
vorobeyme <vorobeyme@gmail.com>
2015-10-05 00:45:38 +0300

    Add config file

---
+++ app/js/config.js
@@ -0,0 +1,11 @@
+// Application configuration
+(function () {
+
+    // Server address
+    window.SERVER_URL = 'http://146.185.190.210:3000/';
+
+    // Alternate server address
+    window.SERVER_URL_ALIAS = 'http://cleancity.ml/';
+
+})();
+
