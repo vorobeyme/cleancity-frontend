ab3ed91390bb0d8bcd4716902f55e000c6b8f293
vorobeyme <vorobeyme@gmail.com>
2015-10-05 00:41:36 +0300

    Add location collection

--- app/js/collections/locations.js
+++ app/js/collections/locations.js
@@ -1,1 +1,12 @@
-// collection
+(function () {
+
+    // Location collection (A list of locations)
+    App.Collections.Locations = new Backbone.Collection.extend({
+
+        model: App.Models.Location,
+        url: SERVER_URL + 'locations'
+
+    });
+
+})();
+
