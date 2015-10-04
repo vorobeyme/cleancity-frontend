be813f27d47e2c645d941a90af7380ccc783272e
vorobeyme <vorobeyme@gmail.com>
2015-10-05 00:41:01 +0300

    Add location model

--- app/js/models/location.js
+++ app/js/models/location.js
@@ -1,1 +1,24 @@
-// model
+(function () {
+
+    // Location model
+    App.Models.Location = new Backbone.Model.extend({
+
+        urlRoot: SERVER_URL + 'locations',
+
+        defaults: {
+            _id: null,
+            name: '',
+            address: '',
+            comment: '',
+            phone: '',
+            photos: '',
+            status: 'new',
+            type: '',
+            latitude: '',
+            longitude: ''
+        }
+
+    });
+
+})();
+
